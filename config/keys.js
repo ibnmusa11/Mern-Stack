27e6758ac3abc1191517d95ee1ab56618037d6a6
Mohamed Musa <mohamed.musa831@gmail.com>
2020-04-26 19:28:28 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Mohamed:mutgab11@chef-project-gfbbw.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose= require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/Items')
+
+
+const app = express();
+
+//BodyParser MiddleWire
+app.use(bodyParser.json());
+
+// Db config 
+const db = require('./config/keys').mongoURI
+
+// Connect to db
+
+mongoose
+    .connect(db, { useUnifiedTopology: true })
+    .then(() => console.log('Database is connected'))
+    .catch(err => console.log(err))
+// Use Routes
+
+app.use('/api/items', items);
+
+const port  = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
